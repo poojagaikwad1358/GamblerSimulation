fff7765ad36086006225c4383099896d23d1a1a4
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-31 00:07:44 +0530

    Gambling for single game & checking win or loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,3 +3,21 @@
 #constant initialization
 stake=100;
 bet=1;
+
+
+win=1
+result=$stake
+random=$((RANDOM%2))
+
+#gambling for $1
+if [ $random -eq $win ]
+then
+	echo "Won"
+	result=$(($result + 1))
+	echo "Wining Amount: "$result
+else
+	echo "Lost"
+	result=$(($result - 1))
+	echo "Losing Amount: "$result
+fi
+
